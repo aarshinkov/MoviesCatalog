fbc6987ed231536c23e8b5d3177abcdcaca081b2
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-12-28 17:03:50 +0200

    Added movie title and movie info

--- app/src/main/java/com/aarshinkov/moviescatalog/MainActivity.java
+++ app/src/main/java/com/aarshinkov/moviescatalog/MainActivity.java
@@ -22,22 +22,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvMovieHeading = findViewById(R.id.tvMovieHeader);
-        tvMovieHeading.setText("Some text");
-
-        TextView tvMovieInfo = findViewById(R.id.tvMovieInfo);
-        tvMovieInfo.setText("Comedy | 120 min");
+//        TextView tvMovieHeading = findViewById(R.id.tvMovieHeader);
+//        tvMovieHeading.setText("Some text");
+//
+//        TextView tvMovieInfo = findViewById(R.id.tvMovieInfo);
+//        tvMovieInfo.setText("Comedy | 120 min");
 
 //        ImageView movieHeader = findViewById(R.id.imageViewHeader);
 //        final String movieHeaderImageUrl = "https://wallpaperaccess.com/full/2071603.jpg";
 //        loadImage(movieHeaderImageUrl, movieHeader);
 
-        ImageView moviePoster = findViewById(R.id.imageViewHeader);
-        final String moviePosterImageUrl = "https://images-na.ssl-images-amazon.com/images/I/91FKuRPgwCL._AC_SL1500_.jpg";
-        loadImage(moviePosterImageUrl, moviePoster);
+//        ImageView moviePoster = findViewById(R.id.imageViewHeader);
+//        final String moviePosterImageUrl = "https://images-na.ssl-images-amazon.com/images/I/91FKuRPgwCL._AC_SL1500_.jpg";
+//        loadImage(moviePosterImageUrl, moviePoster);
     }
 
-    private void loadImage(String url, ImageView imageView) {
-        Picasso.get().load(url).into(imageView);
+    private void loadImage(String imgUrl, ImageView imageView) {
+        Picasso.get().load(imgUrl).into(imageView);
     }
 }
